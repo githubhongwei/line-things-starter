43c077f687449db3258572cd22defe82ed221ec0
githubhongwei <45753897+githubhongwei@users.noreply.github.com>
2019-12-02 20:45:39 +0800

    update CHARACTERISTIC_UUID for 每日膝

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,8 +1,8 @@
 // User service UUID: Change this to your generated service UUID
 const USER_SERVICE_UUID         = '0273d730-0736-42db-917a-0369eebd174d'; // 每日膝
 // User service characteristics
-const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
-const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
+const LED_CHARACTERISTIC_UUID   = '0273d730-0003-42db-917a-0369eebd174d'; // 每日膝 Raw 6dof Data
+const BTN_CHARACTERISTIC_UUID   = '0273d730-0004-42db-917a-0369eebd174d'; // 每日膝 Rotation Vector
 
 // PSDI Service UUID: Fixed value for Developer Trial
 const PSDI_SERVICE_UUID         = 'E625601E-9E55-4597-A598-76018A0D293D'; // Device ID
