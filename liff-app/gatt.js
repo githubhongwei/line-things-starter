b9de6bd4c0ee01e8e6580096a65f36e74baa4e1d
10703020 <Sam_TY_Tsai@wistron.com>
2019-12-04 13:59:40 +0800

    Manage codes for certain BLE device.
    1. Add gatt.js.
    2. Update liff.js.
    3. Update index.html.

---
+++ liff-app/gatt.js
@@ -0,0 +1,10 @@
+export const GattService = {
+    DailyKnee: { UUID: '0273d730-0736-42db-917a-0369eebd174d' },
+    DK_DeviceId: { UUID: 'E625601E-9E55-4597-A598-76018A0D293D' }   //TODO: usage?
+}
+
+export const GattCharacteristic = {
+    DK_Six_Dof: { UUID: '02730003-0736-42db-917a-0369eebd174d' },
+    DK_Rotation_Vector: { UUID: '02730004-0736-42db-917a-0369eebd174d' },
+    DK_DeviceId: { UUID: '26E2B12B-85F0-4F3F-9FDD-91D114270E6E' }   //TODO: usage?
+}
--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,16 +1,6 @@
-// User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '0273d730-0736-42db-917a-0369eebd174d'; // 每日膝
-// User service characteristics
-const LED_CHARACTERISTIC_UUID   = '02730003-0736-42db-917a-0369eebd174d'; // 每日膝 Raw 6dof Data
-const BTN_CHARACTERISTIC_UUID   = '02730004-0736-42db-917a-0369eebd174d'; // 每日膝 Rotation Vector
+import { GattService, GattCharacteristic } from './gatt';
 
-// PSDI Service UUID: Fixed value for Developer Trial
-const PSDI_SERVICE_UUID         = 'E625601E-9E55-4597-A598-76018A0D293D'; // Device ID
-const PSDI_CHARACTERISTIC_UUID  = '26E2B12B-85F0-4F3F-9FDD-91D114270E6E';
-
-// UI settings
-let ledState = false; // true: LED on, false: LED off
-let clickCount = 0;
+// Data
 
 // -------------- //
 // On window load //
@@ -24,46 +14,9 @@ window.onload = () => {
 // Handler functions //
 // ----------------- //
 
-function handlerToggleLed() {
-    ledState = !ledState;
-
-    uiToggleLedButton(ledState);
-    liffToggleDeviceLedState(ledState);
-}
-
 // ------------ //
 // UI functions //
 // ------------ //
-
-function uiToggleLedButton(state) {
-    const el = document.getElementById("btn-led-toggle");
-    el.innerText = state ? "Switch LED OFF" : "Switch LED ON";
-
-    if (state) {
-      el.classList.add("led-on");
-    } else {
-      el.classList.remove("led-on");
-    }
-}
-
-function uiCountPressButton() {
-    clickCount++;
-
-    const el = document.getElementById("click-count");
-    el.innerText = clickCount;
-}
-
-function uiToggleStateButton(pressed) {
-    const el = document.getElementById("btn-state");
-
-    if (pressed) {
-        el.classList.add("pressed");
-        el.innerText = "Pressed";
-    } else {
-        el.classList.remove("pressed");
-        el.innerText = "Released";
-    }
-}
 
 function uiToggleDeviceConnected(connected) {
     const elStatus = document.getElementById("status");
@@ -129,15 +82,17 @@ function makeErrorMsg(errorObj) {
 // -------------- //
 
 function initializeApp() {
-    liff.init(() => initializeLiff(), error => uiStatusError(makeErrorMsg(error), false));
+    liff.init(() => initializeLiff(), displayGeneralError);
+}
+
+function displayGeneralError(error) {
+    uiStatusError(makeErrorMsg(error), false);
 }
 
 function initializeLiff() {
     liff.initPlugins(['bluetooth']).then(() => {
         liffCheckAvailablityAndDo(() => liffRequestDevice());
-    }).catch(error => {
-        uiStatusError(makeErrorMsg(error), false);
-    });
+    }).catch(displayGeneralError);
 }
 
 function liffCheckAvailablityAndDo(callbackIfAvailable) {
@@ -150,17 +105,13 @@ function liffCheckAvailablityAndDo(callbackIfAvailable) {
             uiStatusError("Bluetooth not available", true);
             setTimeout(() => liffCheckAvailablityAndDo(callbackIfAvailable), 10000);
         }
-    }).catch(error => {
-        uiStatusError(makeErrorMsg(error), false);
-    });;
+    }).catch(displayGeneralError);
 }
 
 function liffRequestDevice() {
     liff.bluetooth.requestDevice().then(device => {
         liffConnectToDevice(device);
-    }).catch(error => {
-        uiStatusError(makeErrorMsg(error), false);
-    });
+    }).catch(displayGeneralError);
 }
 
 function liffConnectToDevice(device) {
@@ -172,16 +123,9 @@ function liffConnectToDevice(device) {
         uiToggleDeviceConnected(true);
 
         // Get service
-        device.gatt.getPrimaryService(USER_SERVICE_UUID).then(service => {
-            // liffGetUserService(service);
-        }).catch(error => {
-            uiStatusError(makeErrorMsg(error), false);
-        });
-        device.gatt.getPrimaryService(PSDI_SERVICE_UUID).then(service => {
-            liffGetPSDIService(service);
-        }).catch(error => {
-            uiStatusError(makeErrorMsg(error), false);
-        });
+        device.gatt.getPrimaryService(GattService.DailyKnee.UUID).then(service => {
+            liffGetUserService(service);
+        }).catch(displayGeneralError);
 
         // Device disconnect callback
         const disconnectCallback = () => {
@@ -191,81 +135,32 @@ function liffConnectToDevice(device) {
             // Remove disconnect callback
             device.removeEventListener('gattserverdisconnected', disconnectCallback);
 
-            // Reset LED state
-            ledState = false;
-            // Reset UI elements
-            uiToggleLedButton(false);
-            uiToggleStateButton(false);
+            // Reset data & UI
 
             // Try to reconnect
             initializeLiff();
         };
 
         device.addEventListener('gattserverdisconnected', disconnectCallback);
-    }).catch(error => {
-        uiStatusError(makeErrorMsg(error), false);
-    });
+    }).catch(displayGeneralError);
 }
 
 function liffGetUserService(service) {
-    // Button pressed state
-    service.getCharacteristic(BTN_CHARACTERISTIC_UUID).then(characteristic => {
-        liffGetButtonStateCharacteristic(characteristic);
-    }).catch(error => {
-        uiStatusError(makeErrorMsg(error), false);
-    });
-
-    // Toggle LED
-    service.getCharacteristic(LED_CHARACTERISTIC_UUID).then(characteristic => {
-        window.ledCharacteristic = characteristic;
-
-        // Switch off by default
-        liffToggleDeviceLedState(false);
-    }).catch(error => {
-        uiStatusError(makeErrorMsg(error), false);
-    });
+    // [DailyKnee] 6-dof data
+    service.getCharacteristic(GattCharacteristic.DK_Six_Dof.UUID).then(characteristic => {
+        liffGetDkSixDofCharacteristic(characteristic);
+    }).catch(displayGeneralError);
 }
 
-function liffGetPSDIService(service) {
-    // Get PSDI value
-    service.getCharacteristic(PSDI_CHARACTERISTIC_UUID).then(characteristic => {
-        return characteristic.readValue();
-    }).then(value => {
-        // Byte array to hex string
-        const psdi = new Uint8Array(value.buffer)
-            .reduce((output, byte) => output + ("0" + byte.toString(16)).slice(-2), "");
-        document.getElementById("device-psdi").innerText = psdi;
-    }).catch(error => {
-        uiStatusError(makeErrorMsg(error), false);
-    });
-}
-
-function liffGetButtonStateCharacteristic(characteristic) {
-    // Add notification hook for button state
-    // (Get notified when button state changes)
+function liffGetDkSixDofCharacteristic(characteristic) {
+    var rawDataText = document.getElementById("device-raw-data");
+    // Add notification hook
     characteristic.startNotifications().then(() => {
         characteristic.addEventListener('characteristicvaluechanged', e => {
-            const val = (new Uint8Array(e.target.value.buffer))[0];
-            if (val > 0) {
-                // press
-                uiToggleStateButton(true);
-            } else {
-                // release
-                uiToggleStateButton(false);
-                uiCountPressButton();
-            }
-        });
-    }).catch(error => {
-        uiStatusError(makeErrorMsg(error), false);
-    });
-}
+            const bytes = new Uint8Array(e.target.value.buffer);
+            rawDataText.innerText = bytes.map(v => v.toString()).join(', ');
 
-function liffToggleDeviceLedState(state) {
-    // on: 0x01
-    // off: 0x00
-    window.ledCharacteristic.writeValue(
-        state ? new Uint8Array([0x01]) : new Uint8Array([0x00])
-    ).catch(error => {
-        uiStatusError(makeErrorMsg(error), false);
-    });
+            //TODO: real data not extracted yet.
+        });
+    }).catch(displayGeneralError);
 }
